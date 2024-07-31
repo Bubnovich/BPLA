edba4011de854261984d00b2684607f45b1e7ed5
Bubnovich <bubnovichmail@mail.ru>
2024-07-31 17:33:34 +0600

    Реализация защиты соединений

---
+++ Modul_2/practise_2_15_Server.py
@@ -0,0 +1,26 @@
+# pip install PyJWT
+import  jwt
+import datetime
+
+SECRET_KEY = 'myKEY-111'
+
+def genetate_token(user_id):
+    payload = {
+        'user_id': user_id,
+        "exp": datetime.datetime.utcnow() + datetime.timedelta(seconds=5)
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+    return token
+
+
+def get_token(user_id):
+    if user_id:
+        token = genetate_token(user_id)
+        print(token)
+        return token
+    return None
+
+if __name__ == "__main__":
+    user_id = "egor1"
+    token = genetate_token(user_id)
+    print(token)
