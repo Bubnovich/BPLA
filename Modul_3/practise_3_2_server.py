35758e0cdd82760521acf8eb4b5ce50ef8bf1c4e
Bubnovich <bubnovichmail@mail.ru>
2024-08-08 16:46:03 +0600

    Добавляем реализацию вывода информации в браузер

---
+++ Modul_3/practise_3_2_server.py
@@ -0,0 +1,56 @@
+#opencv-python
+from flask import Flask, request, jsonify
+import cv2
+import numpy as np
+
+app = Flask(__name__)
+
+drone_state = {
+    "status": "landed",
+    "position": {
+        "latitude": 0.0,
+        "longitude": 0.0,
+        "altitude": 0.0,
+    },
+    "telemetry_data": []
+}
+
+video_frame = None
+
+@app.route("/drone/takeoff", methods=["POST"])
+def takeoff():
+    try:
+        if drone_state["status"] == "flying":
+            return jsonify({"error": "Дрон уже в воздухе"}), 400
+
+        drone_state["status"] = "flying"
+        drone_state["position"]["altitude"] = 10.0
+
+        app.logger.info("Дрон взлетел")
+        return jsonify({
+            "message": "Взлет выполнен",
+            "drone_state": drone_state
+        }), 200
+    except Exception as e:
+        app.logger.error(f"Ошибка при взлете: {e}")
+        return jsonify({"error": "Ошибка при взлете"}), 500
+
+@app.route("/telemetry", methods=["POST"])
+def receive_telemetry():
+    data = request.json()
+    drone_state["telemetry_data"].append(data)
+    return jsonify({"status": "Получили"}), 200
+
+@app.route("/display", method=["GET"])
+def display_telemetry():
+    return jsonify(drone_state["telemetry_data"]), 200
+
+@app.route("/video", method=["POST"])
+def receive_video():
+    global video_frame
+    np_array = np.frombuffer(request.data, dtype=np.uint8)
+    video_frame = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
