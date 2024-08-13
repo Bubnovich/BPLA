b8acb87cba39508993c0cab5df7a06d113d2da60
Bubnovich <bubnovichmail@mail.ru>
2024-08-13 17:39:48 +0600

    Работа с библиотекой websockets

---
+++ Modul_3/practise_3_6_drone_server.py
@@ -0,0 +1,19 @@
+# pip install websockets asyncio
+import websockets
+import asyncio
+
+async def control_dron(websocket, path):
+    try:
+        async for msg in websocket:
+            print(f"Получена команда: {msg}")
+            if msg == "takeoff":
+                print(f"Дрон взлетает")
+            elif msg == "land":
+                print(f"Дрон приземляется")
+    except websockets.ConnectionClosed as e:
+        print(f"Соединение закрыто: {e}")
+    except Exception as e:
+        print(e)
+start_server = websockets.serve(control_dron, "localhost", 8765)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
