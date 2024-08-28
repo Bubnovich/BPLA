0054495cf01ad9954c65f2c4252f5a9b26468f97
Bubnovich <bubnovichmail@mail.ru>
2024-08-28 16:32:33 +0600

    Реализация паттерна абстрактная фабрика, комманда, конвеер

--- Modul_3/practise_3_13.py
+++ Modul_3/practise_3_13.py
@@ -4,6 +4,8 @@ class DroneController:
     def takeoff(self):
         print('Дрон взлетает')
 
+    def move_forward(self, distance: float):
+       print(f"Летим вперед на {distance} меторов")
 
 class ICommand(ABC):
     @abstractmethod
@@ -23,5 +25,19 @@ class MoveForward(ICommand):
                  distance: float
                  ):
         self.__drone = drone
+        self.distance = distance
     def execute(self):
-        self.__drone.move_forward()
+        self.__drone.move_forward(self.distance)
+
+class IFlightStrategy(ABC):
+    @abstractmethod
+    def execute(self, commands: list):
+        pass
+
+#Стратегия разведывательной миссии
+class ReconMissionStrategy(IFlightStrategy):
+    def execute(self, commands: list):
+        print(f"Начало выполнения разведывательной миссии")
+        for command in commands:
+            command.execute()
+        print(f"Конец миссии")
--- Modul_3/practise_3_13_test.py
+++ Modul_3/practise_3_13_test.py
@@ -1,5 +1,18 @@
 from practise_3_13 import *
 
 drone_controller = DroneController()
+
 takeoff = Takeoff(drone_controller)
-takeoff.execute()
+move_forward = MoveForward(drone_controller, 100)
+
+#takeoff.execute()
+#move_forward.execute()
+
+commands = [
+    Takeoff(drone_controller),
+    MoveForward(drone_controller, 100),
+MoveForward(drone_controller, -100)
+]
+
+strategy = ReconMissionStrategy()
+strategy.execute(commands=commands)
