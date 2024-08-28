1ae8f57ae18ac7365ab6ea2091986f6a0bd4ed6e
Bubnovich <bubnovichmail@mail.ru>
2024-08-28 17:03:44 +0600

    Реализация паттерна абстрактная фабрика, комманда, стратегия

--- Modul_3/practise_3_13.py
+++ Modul_3/practise_3_13.py
@@ -7,6 +7,9 @@ class DroneController:
     def move_forward(self, distance: float):
        print(f"Летим вперед на {distance} меторов")
 
+    def turn(self,degree: float):
+        print(f"Поворачиваем на {degree} градусов")
+
 class ICommand(ABC):
     @abstractmethod
     def execute(self):
@@ -29,6 +32,16 @@ class MoveForward(ICommand):
     def execute(self):
         self.__drone.move_forward(self.distance)
 
+class Turn(ICommand):
+    def __init__(self, drone: DroneController,
+                 degree: float
+                 ):
+        self.__drone = drone
+        self.degree = degree
+    def execute(self):
+        self.__drone.turn(self.degree)
+
+
 class IFlightStrategy(ABC):
     @abstractmethod
     def execute(self, commands: list):
@@ -40,4 +53,32 @@ class ReconMissionStrategy(IFlightStrategy):
         print(f"Начало выполнения разведывательной миссии")
         for command in commands:
             command.execute()
+        print(f"Конец миссии")
+
+
+class DroneContext:
+    def __init__(self, strategy: IFlightStrategy=None):
+        self.__strategy = strategy
+        self.__commands = []
+
+    def set_strategy(self, strategy:IFlightStrategy):
+        self.__strategy = strategy
+
+    def add_command(self, command: ICommand):
+        self.__commands.append(command)
+
+    def execute(self):
+        self.__strategy.execute(self.__commands)
+        self.__commands.clear()
+
+class PatrolMissionStrategy(IFlightStrategy):
+    def __init__(self, n_patrols: int):
+        self.__n_patrols = n_patrols
+
+    def execute(self, commands: list):
+        print(f"Начало выполнения разведывательной миссии")
+        for _ in range(self.__n_patrols):
+            for command in commands:
+                command.execute()
+            print(f"Патрулирование выполнено")
         print(f"Конец миссии")
--- Modul_3/practise_3_13_test.py
+++ Modul_3/practise_3_13_test.py
@@ -2,17 +2,33 @@ from practise_3_13 import *
 
 drone_controller = DroneController()
 
-takeoff = Takeoff(drone_controller)
-move_forward = MoveForward(drone_controller, 100)
+# takeoff = Takeoff(drone_controller)
+# move_forward = MoveForward(drone_controller, 100)
+#
+# takeoff.execute()
+# move_forward.execute()
+#
+# commands = [
+#     Takeoff(drone_controller),
+#     MoveForward(drone_controller, 100),
+# MoveForward(drone_controller, -100)
+# ]
+#
+# strategy = ReconMissionStrategy()
+# strategy.execute(commands=commands)
 
-#takeoff.execute()
-#move_forward.execute()
+context = DroneContext()
+context.set_strategy(ReconMissionStrategy())
 
-commands = [
-    Takeoff(drone_controller),
-    MoveForward(drone_controller, 100),
-MoveForward(drone_controller, -100)
-]
+context.add_command(Takeoff(drone_controller))
+context.add_command(MoveForward(drone_controller, 100))
+context.add_command(MoveForward(drone_controller, 20))
 
-strategy = ReconMissionStrategy()
-strategy.execute(commands=commands)
+context.execute()
+
+context.set_strategy(PatrolMissionStrategy(n_patrols=3))
+for _ in range(4):
+    context.add_command(MoveForward(drone_controller, 50))
+    context.add_command(Turn(drone_controller, 90))
+
+context.execute()
